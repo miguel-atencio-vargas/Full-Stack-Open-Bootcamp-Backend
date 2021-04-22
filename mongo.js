56ba3fb30f78bb5f34235812ab8ab37095b445b2
skyfall947 <atencio947@gmail.com>
2021-04-22 18:07:06 -0400

    3.12: Base de datos de línea de comandos
    installed mongoose too

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1);
+}
+
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url =
+  `mongodb+srv://fullstack_user:${password}@cluster0.2w4jk.mongodb.net/phonebook-app?retryWrites=true`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if(!name || !number) {
+  console.log('phonebook');
+  Person.find({}).then(result => {
+    result.forEach(person => console.log(`${person.name} ${person.number}`));
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({ name, number });
+  person.save().then(result => {
+    // change the format output
+    console.log(`added ${result.name} ${result.number} to phonebook`);
+    mongoose.connection.close()
+  })
+}
+
