1c8ca82fb3554646c0dc181b8d298265a462d3fc
skyfall947 <atencio947@gmail.com>
2021-04-22 23:28:44 -0400

    3.13: Base de datos de la agenda telefónica, paso 1
    installed mongoose too

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+'use strict';
+if(process.env.NODE_ENV !== 'production') require ('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
+const Person = require('./models/people');
 const app = express();
 
 
@@ -13,43 +15,40 @@ app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :b
 morgan.token('body', req => JSON.stringify(req.body))
   
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": "1"
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": "2"
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": "3"
-  },
-  {
-    "name": "Miguel",
-    "number": "99999",
-    "id": "4"
-  },
-  {
-    "name": "Juan",
-    "number": "12345",
-    "id": "5"
-  },
-  {
-    "name": "John Doe",
-    "number": "342423423",
-    "id": "6"
-  }
-];
+// let persons = [
+//   {
+//     "name": "Arto Hellas",
+//     "number": "040-123456",
+//     "id": "1"
+//   },
+//   {
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523",
+//     "id": "2"
+//   },
+//   {
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345",
+//     "id": "3"
+//   },
+//   {
+//     "name": "Miguel",
+//     "number": "99999",
+//     "id": "4"
+//   },
+//   {
+//     "name": "Juan",
+//     "number": "12345",
+//     "id": "5"
+//   },
+//   {
+//     "name": "John Doe",
+//     "number": "342423423",
+//     "id": "6"
+//   }
+// ];
 
-const genereteID = () => '' +Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
-
-app.get('/api/persons', (req, res) => res.json(persons));
-
+// get info about the API
 app.get('/api/info', (req, res) => {
   const HTMLdata = `
     <p>Phonebook has info for ${persons.length} people.</p>
@@ -57,44 +56,50 @@ app.get('/api/info', (req, res) => {
   res.send(HTMLdata)
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
-  const person = persons.find(item=> item.id === id);
-  if(!person) return res.status(404).end();
-  res.json(person);
+// endpoint for get all the contacts
+app.get('/api/people', (req, res) => {
+  Person.find({}).then(people => res.json(people))
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
-  persons = persons.filter(item => item.id !== id);
-  res.status(204).end();
+// get only one contact provided an id
+app.get('/api/people/:id', (req, res) => {
+  console.log(req.params.id);
+  Person.findById(req.params.id)
+  .then(person => res.json(person))
+  .catch(err => res.status(404).send({
+    message: 'not found contact with provided id'
+  }));
 });
 
-app.post('/api/persons', (req, res) => {
-  const body = req.body;
-  if(!body.name || !body.number) return res.status(400).json({
-    error: 'contact data is not provided'
-  });
-  const samePerson = persons.find(item => item.name.toLocaleLowerCase() === body.name.toLocaleLowerCase());
-  if(samePerson) return res.status(400).json({
-    error: `${body.name} is already registered. Name must be unique`
-  });
-  const newPerson = {
-    id: genereteID(),
-    name: body.name,
-    number: body.number
-  }
-  persons = persons.concat(newPerson);
-  res.json(newPerson);
-});
+// app.delete('/api/people/:id', (req, res) => {
+//   const id = req.params.id;
+//   persons = persons.filter(item => item.id !== id);
+//   res.status(204).end();
+// });
+
+// app.post('/api/people', (req, res) => {
+//   const body = req.body;
+//   if(!body.name || !body.number) return res.status(400).json({
+//     error: 'contact data is not provided'
+//   });
+//   const samePerson = persons.find(item => item.name.toLocaleLowerCase() === body.name.toLocaleLowerCase());
+//   if(samePerson) return res.status(400).json({
+//     error: `${body.name} is already registered. Name must be unique`
+//   });
+//   const newPerson = {
+//     id: genereteID(),
+//     name: body.name,
+//     number: body.number
+//   }
+//   persons = persons.concat(newPerson);
+//   res.json(newPerson);
+// });
 
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({error: 'unknow endpoint'})
 }
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log('Server listen on:', PORT);
-});
+app.listen(PORT, () => console.log('Server listen on:', PORT));
